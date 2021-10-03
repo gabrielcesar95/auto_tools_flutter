566cba9590551b29f5a4a0e2825c8a26d360a2f1
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2021-10-03 16:37:04 -0300

    fix(FuelComparer): 💄 return results as AlertDialog

--- lib/pages/tools/fuel-comparer/widgets/results.dart
+++ lib/pages/tools/fuel-comparer/widgets/results.dart
@@ -14,52 +14,36 @@ class Results extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF737373),
-      // height: 240,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Theme.of(context).canvasColor,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(8),
-              topRight: Radius.circular(8),
-            )),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+    return AlertDialog(
+      title: const Text(
+        'Combustível recomendado',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                'Combustível recomendado',
-                style: TextStyle(
-                  fontSize: 20,
+              Text(
+                fuelType.title,
+                style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24,
                 ),
               ),
-              SizedBox.fromSize(
-                size: const Size.fromHeight(8),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    fuelType.title,
-                    style: const TextStyle(
-                      color: Colors.blue,
-                      fontSize: 24,
-                    ),
-                  ),
-                  Text(
-                    'Preço por Km: R\$ ${formatPricePerLiter()}',
-                    style: const TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
+              Text(
+                'Preço por Km: R\$ ${formatPricePerLiter()}',
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
